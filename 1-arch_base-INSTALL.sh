e8562e7b7b3053854f851d1bb1a2c97948330fc6
dr0x <61434992+dellaub@users.noreply.github.com>
2020-04-10 02:30:20 +0200

    Update 1-arch_base-INSTALL.sh

--- 1-arch_base-INSTALL.sh
+++ 1-arch_base-INSTALL.sh
@@ -97,9 +97,43 @@ Mountfilesystem
 movescriptscd 
 
 # iso 04 [set mirrorlist]
+		
+		setmirrors(){
 
-		echo -e " \033[1;33m[ STEP 04 - SETTING MIRRORLIST ]\033[0m  "
-		sleep 1
+			echo -e " \033[1;33m[ STEP 04 - SETTING MIRRORS ] \033[0m  "
+			sleep 1
+
+				# go to mirror list location
+					cd /etc/pacman.d/
+				# create header
+					sed '5q' > fastmirrors
+				# list the closest countries
+
+					echo -e "\033[32m[ name of your country (i.e : France , Belgium) ]\033[0m"
+						read country
+
+					echo -e "\033[32m[ what is THE CLOSEST country to yours? ]\033[0m"
+						read close1
+
+					echo -e "\033[32m[ what is THE SECOND CLOSEST country to yours? ]\033[0m"
+						read close2
+
+					echo -e "\033[32m[ what is THE THIRD CLOSEST country to yours? ]\033[0m"
+						read close3				
+				
+				#append countries selected to fastmirrors
+					grep -A 1 "$country" | tr -d '-' >> fastmirrors
+					grep -A 1 "$close1" | tr -d '-' >> fastmirrors
+					grep -A 1 "$close2" | tr -d '-' >> fastmirrors
+					grep -A 1 "$close3" | tr -d '-' >> fastmirrors	
+				# backup mirrorlist file
+					cp mirrorlist mirrorlist.bak
+				# enable fastmirrors file
+					mv fastmirrors mirrorlist
+}
+
+setmirrors
+
 
 # iso 05 [install kernel and firmware]
 
