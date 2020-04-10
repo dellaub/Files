13596fac12ac629ad38f52ed86c8488addded9e2
dr0x <61434992+dellaub@users.noreply.github.com>
2020-04-10 05:10:53 +0200

    Update 2-arch_base-INSTALL.sh

--- 2-arch_base-INSTALL.sh
+++ 2-arch_base-INSTALL.sh
@@ -243,18 +243,11 @@ configureDE
 
 echo -e " \033[1;33m\033[44m[ PLEASE, WHEN YOU LOG BACK TO THE USB: ]\033[0m "
 sleep 2
-echo -e " \033[1;33m\033[44m[ 1 RUN THIS COMMAND $\033[1;37m umount -r /mnt \033[0m\033[1;33m]\033[0m "
+echo -e " \033[1;33m\033[44m[ 1 RUN THIS COMMAND $\033[1;37m exit \033[0m\033[1;33m]\033[0m "
 sleep 2
-echo -e " \033[1;33m\033[44m[ 2 THEN , THIS COMMAND $\033[1;37m reboot \033[0m\033[1;33m]\033[0m "
+echo -e " \033[1;33m\033[44m[ 2 THEN , THIS COMMAND $\033[1;37m umount -R /mnt \033[0m\033[1;33m]\033[0m "
+sleep 2
+echo -e " \033[1;33m\033[44m[ 3 THEN , THIS COMMAND $\033[1;37m reboot \033[0m\033[1;33m]\033[0m "
 
 
-exitsys(){
-		
-		echo -e " \033[1;33m[ STEP 15 - EXIT SYSTEM ]\033[0m  "
-		sleep 1
 
-			# exit back to usb
-				exit
-}
-
-exitsys
